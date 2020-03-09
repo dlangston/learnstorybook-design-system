f5337e9afefd26b6864e18bc1121fbb24ffbead7
me <me@arlingtonnuca.ezesoft.net>
2020-03-09 14:42:24 -0400

    initial setup of storybook

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // automatically import all files ending in *.stories.js|mdx
+  stories: ['../src/components/Intro.stories.mdx', '../src/**/*.stories.(js|mdx)'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-storysource',
+    '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+  ],
+};
